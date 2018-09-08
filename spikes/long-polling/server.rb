daaf196c329f119fc0dfa8b8d6a2877f58caa840
Wojtek Majewski <jumski@gmail.com>
2018-09-08 05:55:48 +0200

    More sophisticated server

--- spikes/long-polling/server.rb
+++ spikes/long-polling/server.rb
@@ -1,6 +1,30 @@
 require 'bundler'
 Bundler.require
 
+require 'yaml/store'
+class Store < DelegateClass(YAML::Store)
+  class << self
+    attr_accessor :pstore, :mutex
+  end
+
+  def initialize(path)
+    self.class.pstore ||= PStore.new(path)
+    self.class.mutex ||= Mutex.new
+
+    super self.class.pstore
+  end
+
+  def transaction(&block)
+    self.class.mutex.synchronize { super(&block) }
+  end
+end
+
+helpers do
+  def store
+    Store.new('test.store')
+  end
+end
+
 use Rack::Logger
 
 get '/' do
@@ -8,9 +32,21 @@ get '/' do
 end
 
 get '/poll' do
-  request.logger.info "sleep STTART"
   sleep 1
-  request.logger.info 'sleep END'
 
-  DateTime.now.iso8601.to_json
+  store.transaction do
+    request.logger.info store[:state]
+    store[:state]
+  end.to_json
+end
+
+post '/state' do
+  # sleep 5
+
+  request.body.rewind
+  json = JSON.parse(request.body.read)
+
+  store.transaction do
+    store[:state] = json
+  end.to_json
 end
