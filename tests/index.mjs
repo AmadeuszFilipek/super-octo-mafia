a570d24a60e8865030c37468e9c12b3f202a1ce3
Wojtek Majewski <jumski@gmail.com>
2018-10-12 23:03:01 +0200

    Two users test

--- tests/index.mjs
+++ tests/index.mjs
@@ -4,16 +4,19 @@ import puppeteer from 'puppeteer';
 async function perform(browser, site) {
   const page = await browser.newPage();
   await page.goto(`https://${site}.com`);
-  await page.screenshot({path: `${site}.png`});
+  await page.screenshot({path: `tmp/${site}.png`});
 }
 
 (async () => {
-  const b_a = await puppeteer.launch();
-  const b_b = await puppeteer.launch();
+  const b_a = await puppeteer.launch({ context: 'default', headless: false });
+  const b_b = await puppeteer.launch({ context: 'default', headless: false });
+  const b_c = await puppeteer.launch({ context: 'default', headless: false });
 
   await perform(b_a, 'google');
   await perform(b_b, 'github');
+  await perform(b_c, 'bing');
 
   await b_a.close();
   await b_b.close();
+  await b_c.close();
 })();
