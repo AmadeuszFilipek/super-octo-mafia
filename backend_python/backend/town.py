ddaee90d85a3dd48050a0708e64d3fc8fcc28122
Amadeusz <30732784+AmadeuszFilipek@users.noreply.github.com>
2018-10-13 15:29:13 +0200

    Backend (#9)
    * Added backdoor endpoint
    
    * General restructure of project files. Backend abractized into classes. Town class and Player class implemented.
    
    * Rename mafia to bakckend_python, move all backend files to that
    
    * Remove also "started_at" from json
    
    * Responses updated
    
    * Requests and responses updated.
    
    * Remove old files
    
    * Remove more old code
    
    * Update tests.sh comment

---
+++ backend_python/backend/town.py
@@ -0,0 +1,72 @@
+from backend_python.backend.player import Player
+from flask import jsonify
+from datetime import datetime
+
+class Town(object):
+
+	def __init__(self, slug):
+		self.slug = slug
+		self.players = {}
+		self.votes = {}
+		self.state = {'id': 'waiting_for_players'}
+		self.started_at = datetime.now().timestamp()
+
+
+	def to_dict(self):
+		dictionary = self.__dict__.copy()
+		dictionary['players'] = {}
+
+		for player in self.players.keys():
+			dictionary['players'][player] = self.players[player].to_dict()
+
+		return dictionary
+
+
+	def add_player(self, name, is_host = False):
+		self.players[name] = Player(name, is_host)
+
+
+	def start_game(self, backdoor_players = {}):
+		for player in self.players.values():
+			try:
+				player.character = backdoor_players[player.name]
+			except KeyError:
+				player.generate_character()
+			
+		self.state['id'] = 'day_voting'
+
+
+	def vote(self, vote):
+		self.votes[vote['voterName']] = vote['voteeName']
+
+
+	def execute_vote(self):
+		vote_counts = {}
+		for player in self.players.values():
+			vote_counts[sum(
+				[vote for vote in self.votes.values() if vote == player.name])] \
+				= player.name
+
+		player_to_die = vote_counts[max(vote_counts.keys())]
+		player_to_die.kill()
+
+		self.state = {'id': 'night_voting'}
+
+
+	def jversonify(self):
+		self.version = datetime.now().timestamp()
+
+		return(jsonify(self.to_dict()))
+
+
+	def check_if_game_ended(self):
+		raise NotImplementedError
+
+
+class TownDoesNotExistException(Exception):
+	pass
+
+
+
+
+
