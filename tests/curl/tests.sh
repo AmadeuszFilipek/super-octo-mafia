d5abdd89487ad7424329d0987818460943b5a12d
Wojtek Majewski <jumski@gmail.com>
2018-10-13 03:05:58 +0200

    Guard from http and json errors when fetching old api responses

--- tests/curl/tests.sh
+++ tests/curl/tests.sh
@@ -6,6 +6,39 @@ old_api="$OLD_API_URL"
 new_api="$NEW_API_URL"
 [ -z $new_api ] && new_api="http://localhost:5000"
 
+c_reset="\e[0m"
+c_red="\e[31m"
+c_green="\e[32m"
+
+function run_step() {
+  local api_url=$1
+  local step_filename=$2
+  local response=$(./$step_filename "$api_url" 2>1)
+
+  if [ $? != 0 ]; then
+    echo -e ": ${c_red}FAILED${c_reset}"
+    echo
+    echo "Request to $api_url failed. Response:"
+    echo
+    echo -e "$response"
+    exit 1
+  fi
+
+  local json=$(echo -e $response | python -mjson.tool)
+
+  if [ $? != 0 ]; then
+    echo -e ": ${c_red}FAILED${c_reset}"
+    echo
+    echo "JSON parse error. Response:"
+    echo
+    echo -e "$response"
+    exit 1
+  fi
+
+  # remove "version" to not trigger git diff
+  echo -e "$json" | sed '/"version":/d'
+}
+
 for step_filename in steps/*; do
   step_name=`basename $step_filename`
   step_name=${step_name#step_}
@@ -14,24 +47,24 @@ for step_filename in steps/*; do
 
   echo -n "--- $step_name"
 
-  if [ ! -f $response_path ]; then
-    old_api_response=$(./$step_filename "$old_api" 2>1)
-
-    if [ $? = 0 ]; then
-      echo -e "$old_api_response" > $response_path
-    else
-      echo ': FAILED'
-      echo
-      echo "Request to old api failed. Response:"
-      echo
-      echo -e "$old_api_response"
-      exit 1
-    fi
+  if [ -f $response_path ]; then
+    old_api_json=$(cat $response_path)
+  else
+    old_api_json=$(run_step $old_api $step_filename | tee $response_path)
   fi
 
-  diff --color=always <(cat $response_path  | python -mjson.tool| sed '/"version":/d') <(
-    ./$step_filename "$new_api" 2>1 | python -mjson.tool | sed '/"version":/d'
-  ) | sed 's/^/    /'
+  new_api_json=$(run_step $new_api $step_filename)
 
-  echo ': OK'
+  diff_output=$(
+    diff --color=always <(echo -e "$old_api_json") <(echo -e "$new_api_json")
+  )
+
+  if [ $? = 0 ]; then
+    echo -e ": ${c_green}OK${c_reset}"
+  else
+    echo -e ": ${c_red}FAILED${c_reset}"
+    echo
+    echo -e "$diff_output" | sed 's/^/    /'
+    exit 1
+  fi
 done
