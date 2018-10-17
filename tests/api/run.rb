acca1c97437b0066d45b6212e72a3e6db23317bb
Wojtek Majewski <jumski@gmail.com>
2018-10-17 23:12:34 +0200

    Working Parser and Dumper

--- tests/api/run.rb
+++ tests/api/run.rb
@@ -1,7 +1,7 @@
 require 'pp'
 require 'pry'
 
-class RequestReader
+class RequestParser
   def initialize(path)
     @path = path
   end
@@ -24,6 +24,10 @@ class RequestReader
     parts.last[1..-1].join
   end
 
+  def to_h
+    { verb: verb, uri: uri, headers: headers, body: body }
+  end
+
   private
 
   attr_reader :path
@@ -41,4 +45,32 @@ class RequestReader
   end
 end
 
-req = RequestReader.new('./steps/03_create_player')
+class RequestDumper
+  def initialize(verb:, uri:, headers:, body:)
+    @verb = verb
+    @uri = uri
+    @headers = headers
+    @body = body
+  end
+
+  def to_s
+    [
+      "#{verb} #{uri}",
+      headers.map {|k,v| "#{k}: #{v}"},
+      "",
+      body
+    ].flatten.join("\n")
+  end
+
+  private
+
+  attr_reader :verb, :uri, :headers, :body
+end
+
+parser = RequestParser.new('./steps/03_create_player')
+dumper = RequestDumper.new(parser.to_h)
+File.open('./output', 'wb') { |f| f.write(dumper.to_s) }
+
+if File.read('./steps/03_create_player') != dumper.to_s
+  raise "Files are different, run diff on them!"
+end
