5748165bc87cd355b6deb945c556dc990c9d2d33
Wojtek Majewski <jumski@gmail.com>
2018-10-29 21:30:42 +0100

    Remove old files

--- tests/api/lib/scenarios_list_spec.rb
+++ tests/api/lib/scenarios_list_spec.rb
@@ -58,7 +58,7 @@ class ScenariosList
   def all
     leaf_subdirectories.map do |leaf_subdirectory|
       Scenario.from_leaf(root_path, leaf_subdirectory)
-    end
+    end.sort_by(&:name)
   end
 
   private
@@ -76,10 +76,28 @@ describe ScenariosList do
   it "#all returns all scenarios" do
     list = ScenariosList.new('./fixtures/scenarios')
     scenarios = list.all
+
     expect(scenarios.length).to eq 6
+
+    # scenarios have steps pointing to existing files
+    scenarios.each do |scenario|
+      scenario.steps.each do |step|
+        expect(File.exist?(step.request_path)).to be_truthy
+        expect(File.file?(step.request_path)).to be_truthy
+
+        expect(File.exist?(step.response_path)).to be_truthy
+        expect(File.file?(step.response_path)).to be_truthy
+      end
+    end
 
     s1, s2, s3, s4, s5, s6 = scenarios
 
-    binding.pry
+    # scenarios have proper names
+    expect(s1.name).to eq 'error_path/second_level/third_level'
+    expect(s2.name).to eq 'error_path/second_level_2/third_level_2'
+    expect(s3.name).to eq 'happy_path/part1/part1_1'
+    expect(s4.name).to eq 'happy_path/part1/part1_2'
+    expect(s5.name).to eq 'happy_path/part2/part2_1'
+    expect(s6.name).to eq 'happy_path/part2/part2_2'
   end
 end
