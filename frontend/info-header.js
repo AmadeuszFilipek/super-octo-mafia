6ad1aad4f997b47a27dab828081e66d147e45487
Amadeusz Filipek <amadeusz.filipek@gmail.com>
2019-01-20 22:50:01 +0100

    Info-header added.

---
+++ frontend/info-header.js
@@ -0,0 +1,24 @@
+const stateToLabel = {
+    new_town_form: "Create a new game or join existing one.",
+    waiting_for_players: "Waiting for other players",
+    day_voting: "Vote for a player you suspect to be from mafia.",
+    day_results: "The town concil has decided:",
+    night_voting: "Vote for a player to eliminate. Mafia does not pity.",
+    night_results: "The mafia does not sleep and they eliminated:",
+    game_ended: "The game has ended. Well played everyone.",
+}
+
+Vue.component('info-header', {
+    props: ['state'],
+    computed: {
+        text() {
+            console.log(stateToLabel)
+            console.log(stateToLabel[this.state])
+
+            return stateToLabel[this.state];
+        },
+    },
+    template: "<h2 class='info-header'>{{ text }}</h2>"
+  })
+
+export default null;
