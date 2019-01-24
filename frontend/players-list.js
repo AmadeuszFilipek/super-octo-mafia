cdca75eb24f2e235a59027c264b4b07585ad3d6c
Wojtek Majewski <jumski@gmail.com>
2019-01-24 23:02:29 +0100

    Sort players and highlight current player

--- frontend/players-list.js
+++ frontend/players-list.js
@@ -1,9 +1,27 @@
 
 Vue.component('players-list', {
-  props: ['players'],
+  props: ['players', 'currentPlayer'],
+  methods: {
+    isCurrentPlayer(player) {
+      return this.currentPlayer === player;
+    }
+  },
+  computed: {
+    sortedPlayers() {
+      return Object.values(this.players).sort((p1, p2) => {
+        if (this.isCurrentPlayer(p1)) return -1;
+
+        return 1;
+      });
+    }
+  },
   template: `
   <b-list-group id="players">
-    <b-list-group-item v-for="player in players" v-bind:key=player.name>
+    <b-list-group-item
+      v-for="player in sortedPlayers"
+      v-bind:class="{ current: isCurrentPlayer(player) }"
+      v-bind:key=player.name>
+
       <slot v-bind:player=player>
         {{player.name}}
       </slot>
