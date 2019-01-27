cb184f7ba7c6d840cd11aef9602c3575d0b68fdc
Amadeusz Filipek <amadeusz.filipek@gmail.com>
2019-01-27 19:24:40 +0100

    [WIP] Working on the results view.

--- frontend/info-header.js
+++ frontend/info-header.js
@@ -1,4 +1,4 @@
-const stateToLabel = {
+const stateToMafiaLabel = {
     new_town_form: "Create a new game or join existing one.",
     waiting_for_players: "Waiting for other players",
     day_voting: "Vote for a player you suspect to be from mafia.",
@@ -8,11 +8,21 @@ const stateToLabel = {
     game_ended: "The game has ended. Well played everyone.",
 }
 
+const stateToCivilLabel = {
+  new_town_form: "Create a new game or join existing one.",
+  waiting_for_players: "Waiting for other players",
+  day_voting: "Vote for a player you suspect to be from mafia.",
+  day_results: "The town concil has decided:",
+  night_voting: "It's getting late. Close your eyes and wait for the morning alarm.",
+  night_results: "The mafia does not sleep and they eliminated:",
+  game_ended: "The game has ended. Well played everyone.",
+}
+
 Vue.component('info-header', {
     props: ['state'],
     computed: {
         text() {
-            return stateToLabel[this.state];
+            return stateToCivilLabel[this.state];
         },
     },
     template: "<h2 class='info-header'>{{ text }}</h2>"
